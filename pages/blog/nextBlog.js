edf2a12b49cfddea0b3e50e513c8e44d2849d9fb
DESKTOP-EMV2I54\Administrator <l211479976@163.com>
2021-05-10 16:22:53 +0800

    Update next

--- pages/blog/nextBlog.js
+++ pages/blog/nextBlog.js
@@ -1,14 +1,45 @@
+import React from 'react'
 import Link from 'next/link'
 import { withRouter } from 'next/router'
+import axios from 'axios'
 
-const nextBlog = ({router}) => {
-  console.log(router)
-  return (
-    <div>
-      <h3>Next blog {router.query.id}</h3>
-      <Link href="/"><button>返回首页</button></Link>
-    </div>
-  )
+class NextBlog extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      router: props.router,
+      data: props.data.data
+    }
+  }
+  static async getInitialProps () {
+    const promise = new Promise((resolve, reject) => {
+      axios
+        .get('http://www.xpxux.com/api/products/get?pageNumber=1&pageSize=10')
+        .then(res => {
+          let data = {...res.data}
+          resolve({data})
+        })
+    })
+    return await promise
+  }
+
+  render () {
+    return (
+      <div>
+        <h3>Next blog {this.state.router.query.id}</h3>
+        <ul>
+          {
+            this.state.data.product.map((item, index) => {
+              return (
+                <li key={index}>{item.title}</li>
+              )
+            })
+          }
+        </ul>
+        <Link href="/"><button>返回首页</button></Link>
+      </div>
+    )
+  }
 }
 
-export default withRouter(nextBlog)
+export default withRouter(NextBlog)
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,115 @@
+import { useState } from 'react'
+import axios from 'axios'
 import Router from 'next/router'
-import MHeader from '../components/MHeader'
+import Link from 'next/link'
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+import { Button } from 'antd'
+import 'antd/dist/antd.css'
 
-export default function Home() {
-  function goBlog () {
-    Router.push('/blog/nextBlog?id=60')
+// 自定义组件懒加载
+const MHeader = dynamic(import('../components/MHeader'))
+
+import style from '../styles/Home.module.css'
+
+const Home = ({data}) => {
+  const product = data.data.product
+  const [color, setColor] = useState('blue')
+  const changeColor = () => {
+    setColor(color == 'blue' ? 'green' : 'blue')
   }
+  const [time, setTime] = useState()
+  const changeTime = async () => {
+    // 模块懒加载
+    const moment = await import('moment')
+    setTime(moment.default(Date.now()).format())
+  }
+  
+  function goBlog () {
+    Router.push({
+      pathname: '/blog/nextBlog',
+      query: {
+        id: 60
+      }
+    })
+  }
+  
+  Router.events.on('routeChangeStart', (...arg) => {
+    let obj = [...arg]
+    console.log('route change start: ', obj)
+  })
+
+  Router.events.on('routeChangeComplete', (...arg) => {
+    console.log('route change complete: ', ...arg)
+  })
+
+  Router.events.on('beforeHistoryChange', (...arg) => {
+    console.log('before history change', ...arg)
+  })
+
+  Router.events.on('hashChangeStart', (...arg) => {
+    console.log('hash start', ...arg)
+  })
+  Router.events.on('hashChangeComplete', (...arg) => {
+    console.log('hash complete', ...arg)
+  })
 
   return (
     <div>
-      <h1 onClick={goBlog}>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <Head>
+        <title>自定义Head</title>
+      </Head>
+      <h1 className={style.title} onClick={goBlog}>
+        home skip
       </h1>
       <MHeader>next blog</MHeader>
+
+      <Link href="#aaa"><a className="s-btn test">hash skip</a></Link>
+
+      <Button type="primary" onClick={changeColor}>changeColor</Button>
+      <Button onClick={changeTime}>changeTime{time}</Button>
+      <ul>
+        {
+          product.map((item, index) => {
+            return (
+              <li key={index}>{item.title}</li>
+            )
+          })
+        }
+      </ul>
+
+      <style jsx>
+        {`
+          .s-btn {
+            margin: 0 20px;
+            color: ${color};
+          }
+          button {
+            margin: 0 20px;
+          }
+        `}
+      </style>
     </div>
   )
 }
+
+Home.getInitialProps = async () => {
+  const promise = new Promise(resolve => {
+    let params = {
+      pageNumer: 1,
+      pageSize: 3
+    }
+    axios({      
+      method: 'GET',
+      url: 'http://www.xpxux.com/api/products/get',
+      params: params
+    })
+    .then(res => {
+      let data = {...res.data}
+      resolve({data})
+    })
+  })
+  return await promise
+}
+
+export default Home
